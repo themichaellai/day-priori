1cfc29fbb208b9a0347b4c136c32d1f413c1487b
Michael Lai <themichaellai@gmail.com>
2014-09-27 02:34:18 -0400

    Editing

--- index.js
+++ index.js
@@ -1,31 +1,104 @@
 var GoalItem = React.createClass({displayName: 'Goal',
   render: function() {
-    console.log(this.props);
     var numberOfCols = 12 / this.props.number;
     var colClass = 'col-xs-' + numberOfCols;
     return React.DOM.div({
-      className: 'goal-container ' + colClass
-    },
+        className: 'goal-container ' + colClass
+      },
       null,
       React.DOM.div({
-        className: 'goal ' + this.props.specialClass
-      },
-        this.props.name
+          className: 'goal ' + this.props.specialClass,
+        },
+        this.renderText()
       )
-    )
+    );
+  },
+  getInitialState: function() {
+    return {
+      name: this.props.name,
+      editing: false
+    };
+  },
+  componentDidMount: function() {
+    console.log('goal item did mount');
+  },
+  componentDidUpdate: function() {
+    console.log('goal componentDidUpdate');
+    this.props.onGoalNameChange();
+  },
+  renderText: function() {
+    if (this.state.editing) {
+      return React.DOM.form({
+          onSubmit: this.toggleName
+        },
+        null,
+        React.DOM.input({
+           ref: 'goalItemInput',
+           type: 'text',
+           value: this.state.name,
+           onChange: this.handleChange,
+           onSubmit: this.toggleName
+         },
+           null
+         )
+      );
+    } else {
+      return React.DOM.div({
+        ref: 'goalItem',
+        onClick: this.toggleName
+      },
+        this.state.name
+      );
+    }
+  },
+  handleChange: function() {
+    this.setState({name:this.refs.goalItemInput.getDOMNode().value});
+  },
+  toggleName: function(e) {
+    if (e) e.preventDefault();
+    this.setState({editing: !this.state.editing});
   }
 });
 
 var GoalRow = React.createClass({displayName: 'GoalRow',
   render: function() {
     var _that = this;
-    var goalItems = _.times(this.props.number, function(i) {
-      return GoalItem(_.extend(_that.props, {name: 'a name ' + i}));
+    this.goalItems = _.times(this.props.number, function(i) {
+      return GoalItem(_.extend(_that.props, {
+        name: 'a name ' + i,
+        onGoalNameChange: _that.updateGoalText
+      }));
     });
     return React.DOM.div.apply(
       this,
       [{className: 'row goal-row'}, null]
-        .concat(goalItems));
+        .concat(this.goalItems));
+  },
+  componentDidUpdate: function(prevProps, prevState) {
+    console.log('goal row did update');
+  },
+  componentDidMount: function() {
+    console.log('goal row did mount');
+    var goals = $(this.getDOMNode()).find('.goal')
+    this.equalizeHeights(goals, Math.max.apply(
+      null, _.map(goals, function(g) {
+        return $(g).outerHeight(true);
+      })));
+  },
+  updateGoalText: function() {
+    this.equalizeHeights($(this.getDOMNode()).find('.goal'));
+  },
+  equalizeHeights: function(goals, maxHeightIn) {
+    console.log('equalizeHeights');
+    var maxHeight = maxHeightIn || Math.max.apply(
+      null, _.map(goals, function(g) {
+        return $(g).height();
+      }));
+    console.log('maxHeight', maxHeight);
+    _.each(goals, function(g) {
+      console.log('setting to', maxHeight);
+      $(g).height(maxHeight);
+    });
   }
 });
 
